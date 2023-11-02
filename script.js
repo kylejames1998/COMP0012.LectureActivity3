fa189983f8f2ab9529f05386d0808358c937173c
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2023-11-01 18:43:16 -0700

    added all variables and console logs

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let myGreeting = "Hello World!";
 console.log(myGreeting);
 
 const pi = 3.14;
+/* pi = 2.345; */ // cannot reassign const variables.
+
+let myNumber = 12;
+
+console.log(Math.min(myNumber, pi)); // returns 3.14 because it is smaller than 12
